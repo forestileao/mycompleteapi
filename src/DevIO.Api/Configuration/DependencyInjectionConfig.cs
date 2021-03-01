b8ce301f01f1266edec07aae9bc6f727f1d8887b
Foresti Leao <pedroforestileao@gmail.com>
2021-02-28 23:51:33 -0300

    first commit

---
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MyDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            return services;
+        }
+    }
+}
